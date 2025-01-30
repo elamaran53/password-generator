58cbd66dca7b5475b3a0d4353dd22b3a7c3c2b3b
elamaran53 <tamilelamaran53@gmail.com>
2025-01-29 16:39:59 -0800

    files added

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const passwordbox = document.getElementById("password");
+const length = 12;
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allcharacter = uppercase + lowercase + number + symbol;
+
+function createpassword(){
+    let password = "";
+    password += uppercase[Math.floor( Math.random() * uppercase.length)];
+    password += lowercase[Math.floor( Math.random() * lowercase.length)];
+    password += number[Math.floor( Math.random() * number.length)];
+    password += symbol[Math.floor( Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allcharacter[Math.floor(Math.random() * allcharacter.length)];
+
+    }
+    passwordbox.value = password;
+}
+
+
+
+function copypassword(){
+    passwordbox.select();
+    document.execCommand("copy")
+}
